c25c81da6391c9cadfcbb29302779ee7c82ea819
Senka <senka.soic@uns.ac.rs>
2020-07-07 21:29:55 +0200

    Changes for docker added.

--- src/main/java/com/team19/carmicroservice/CarmicroserviceApplication.java
+++ src/main/java/com/team19/carmicroservice/CarmicroserviceApplication.java
@@ -1,13 +1,14 @@
 package com.team19.carmicroservice;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -20,11 +21,27 @@ public class CarmicroserviceApplication {
     @Value("${host}")
     String host;
 
+    @Value("${locationqueue}")
+    String locationQueue;
+
+    @Bean
+    Queue locationQueue() {
+        return new Queue(locationQueue, true);
+    }
+
     // Registrujemo bean koji ce sluziti za konekciju na RabbitMQ na localhostu
     @Bean
     public ConnectionFactory connectionFactory() {
         CachingConnectionFactory connectionFactory = new CachingConnectionFactory(host);
         return connectionFactory;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin() {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory());
+        rabbitAdmin.afterPropertiesSet();
+        rabbitAdmin.declareQueue(locationQueue());
+        return rabbitAdmin;
     }
 
 
